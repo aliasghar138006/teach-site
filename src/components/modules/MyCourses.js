b4fd799592347a300553ecf45f7cbbabb0ef5d17
aliasghar138006 <aliasgharshahneh1380@gmail.com>
2024-03-09 16:31:16 +0330

    add last commit

--- src/components/modules/MyCourses.js
+++ src/components/modules/MyCourses.js
@@ -4,11 +4,14 @@ import Card from "./Card";
 import { e2p } from "@/utils/Operations";
 import Link from "next/link";
 async function MyCourses() {
-  const userInfo = await userData();
+  const { courses } = await userData();
 
   return (
     <div className={styles.container}>
-      {userInfo.courses?.map((item, index) => (
+      {!courses.length && (
+        <div className={styles.message}>دوره ای برای نمایش وجود ندارد</div>
+      )}
+      {courses?.map((item, index) => (
         <Link key={index} href={`/courses/${item.id}`}>
           <div className={styles.item}>
             <span>{e2p(index + 1)}</span>
